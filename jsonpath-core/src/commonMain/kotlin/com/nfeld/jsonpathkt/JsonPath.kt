f42f6c49accee21b58e640838c6f99fc9da55c32
DSinge <dsinger718@gmail.com>
2023-11-08 16:40:37 -0500

    Add option to return single values wrapped in an array (#30)

--- jsonpath-core/src/commonMain/kotlin/com/nfeld/jsonpathkt/JsonPath.kt
+++ jsonpath-core/src/commonMain/kotlin/com/nfeld/jsonpathkt/JsonPath.kt
@@ -1,7 +1,11 @@
 package com.nfeld.jsonpathkt
 
 import com.nfeld.jsonpathkt.json.JsonNode
+import com.nfeld.jsonpathkt.json.JsonType
+import com.nfeld.jsonpathkt.tokens.ArrayAccessorToken
+import com.nfeld.jsonpathkt.tokens.ObjectAccessorToken
 import com.nfeld.jsonpathkt.tokens.Token
+import com.nfeld.jsonpathkt.tokens.WildcardToken
 import kotlin.jvm.JvmInline
 
 @JvmInline
@@ -18,9 +22,29 @@ public value class JsonPath private constructor(
   }
 }
 
-public inline fun <reified T> JsonPath.resolveOrNull(node: JsonNode): T? =
+public inline fun <reified T> JsonPath.resolveOrNull(
+  node: JsonNode,
+  options: ResolutionOptions = ResolutionOptions.Default,
+): T? =
   tokens.fold(
     initial = node,
   ) { valueAtPath: JsonNode?, nextToken: Token ->
     valueAtPath?.let(nextToken::read)
+  }?.let {
+    val isRoot = tokens.isEmpty()
+    val containsWildcard = tokens.any { token -> token is WildcardToken }
+    val lastToken = tokens.lastOrNull()
+    val isAccessingAnObjectOrArray =
+      lastToken is ObjectAccessorToken || lastToken is ArrayAccessorToken
+    val isNodeAnArray = it.type == JsonType.Array
+
+    val wrappingRequired =
+      options.wrapSingleValue &&
+        !containsWildcard &&
+        (isRoot || isAccessingAnObjectOrArray || !isNodeAnArray)
+
+    when {
+      wrappingRequired -> it.copy(element = it.toJsonArray(listOf(it.element)))
+      else -> it
+    }
   }?.element as? T
